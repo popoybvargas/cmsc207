1889b23b78fc7f0478099e14150667ecd98d04cb
Jaime I B. Vargas <popoyvargas@popoyMacMini.local>
2021-03-28 11:53:19 +0800

    Fix(?) #4 css path.

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,7 +7,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>CMSC 207</title>
-	<link rel="stylesheet" href="./css/style.css">
+	<base href="/">
+	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 	<div class="container">
