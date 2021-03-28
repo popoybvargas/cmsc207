1113717667295907571b7fb4981d1549f4862600
Jaime I B. Vargas <popoyvargas@popoyMacMini.local>
2021-03-28 11:38:03 +0800

    Fix css path.

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,10 +7,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>CMSC 207</title>
-	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
-	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
-<!-- <body class="text-center mt-3"> -->
 <body>
 	<div class="container">
 		<a href="/"><header class="text-center">CRUD with PHP & MySQL</header></a>
